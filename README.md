513c0aecef393cc95984e9e90483ff0088a3ead4
b3ko <berkowitz.jon@gmail.com>
2014-05-06 23:07:17 -0400

    initial commit of base site - very rough

---
+++ README.md
@@ -0,0 +1,15 @@
+this webiste will have several different timers and counters on it.
+the user will be able to select which timers they want.
+uses local storage so they can return and have the same counters.
+
+done:
+get a basic bootstrap template up
+added a water consumption counter and hourly reminder to drink
+started a stand up hourly reminder
+
+to do:
+make a generic counter and a generic timer so users can count or time whatever they want
+finish the stand up reminder
+add a 20, 20, 20 eye break reminder
+think of other common reminders and counters
+
---
+++ scripts/click.js
@@ -0,0 +1,104 @@
+var min = 60 * 1000;
+var hour = 60 * min;
+
+$( "#submit" ).on( "click", function( event ) {
+  clickCounter();
+});
+
+$( "#save" ).on( "click", function( event ) {
+	if (document.getElementById("optionsWater").checked)
+	{
+		document.getElementById("water").className = "jumbotron";
+	}
+	else
+	{
+		document.getElementById("water").className = "jumbotron hidden";
+	}
+	
+	if (document.getElementById("optionsStandUp").checked)
+	{
+		document.getElementById("standUp").className = "jumbotron";
+		d = new Date();
+		d = d.setHours(d.getHours() + 1);
+		localStorage.standUp = d.toString();
+		document.getElementById("standIn").innerHTML = parseInt((d - Date.now()) / min) + " minutes";
+		//startTimer();
+	}
+	else
+	{
+		document.getElementById("standUp").className = "jumbotron hidden";
+	}
+});
+
+checkDate();
+document.getElementById("result").innerHTML=localStorage.clickcount;
+
+alertFlag = setTimeout(function(){showAlert()}, hour);
+
+function checkDate() {
+	var d = new Date();
+	d.setHours(0);
+	d.setMinutes(0);
+	d.setSeconds(0);
+	d.setMilliseconds(0);
+	var lsLastVisit = new Date(Date.parse(localStorage.lastVisit));
+	if(lsLastVisit < d)
+	{
+		localStorage.clickcount=0;
+	}
+	
+	if(!localStorage.clickcount)
+	{
+		localStorage.clickcount = 0;
+	}
+	
+	localStorage.lastVisit = new Date().toString();
+}
+
+function startTimer() {
+	//start a timer
+	//wait a minute
+	//update innerHTML of standIn
+	//check if 0 minutes are left...if 0
+		//alert user
+	//else
+		//call startTimer recusively
+		
+}
+
+function clickCounter() {
+	if(typeof(Storage)!=="undefined")
+	{
+		checkDate();
+		if (localStorage.clickcount)
+		{
+			localStorage.clickcount=Number(localStorage.clickcount)+1;
+		}
+		else
+		{
+			localStorage.clickcount=1;
+		}
+		
+		document.getElementById("result").innerHTML=localStorage.clickcount;
+		localStorage.lastVisit = new Date().toString();
+		clearTimeout(alertFlag);
+		alertFlag = setTimeout(function(){showAlert()}, hour);
+	}
+	else
+	{
+		document.getElementById("result").innerHTML="Sorry, your browser does not support web storage...";
+	}
+}
+
+function showAlert(){
+	if(confirm("Drink some water!\nDo you want another reminder in 10 minutes?"))
+	{
+		clearTimeout(alertFlag);
+		alertFlag = setTimeout(function(){showAlert()}, (10 * min));
+	}
+	else
+	{
+		clearTimeout(alertFlag);
+		alert("OK. Reminders are turned off until you refresh the page.");
+	}
+}
